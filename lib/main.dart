642a725403a5aa4ce15d0e5fd04ac50e8a02d936
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2023-03-19 02:26:06 +0600

    A basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -33,41 +33,192 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final FirebaseStorage storage = FirebaseStorage();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Birth Registration Image"),
-      ),
-      body: Column(children: [
-        ElevatedButton(
-            onPressed: () async {
-              final results = await FilePicker.platform.pickFiles(
-                allowMultiple: false,
-                type: FileType.custom,
-                allowedExtensions: ['png', 'jpg', 'jpeg'],
-              );
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            top: -50,
+            left: -50,
+            child: Container(
+              width: 200,
+              height: 200,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Positioned(
+            top: -30,
+            right: -30,
+            child: Container(
+              width: 150,
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 100,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.cloud_upload,
+                  size: 100,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 400,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      final results = await FilePicker.platform.pickFiles(
+                        allowMultiple: false,
+                        type: FileType.custom,
+                        allowedExtensions: ['png', 'jpg', 'jpeg'],
+                      );
 
-              if (results == null) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("No file selected"),
+                      if (results == null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("No file selected"),
+                          ),
+                        );
+                        return null;
+                      }
+
+                      final path = results.files.single.path!;
+                      final fileName = results.files.single.name;
+
+                      storage
+                          .uploadFile(path, fileName)
+                          .then((value) => print("Done"));
+
+                      print(path);
+                      print(fileName);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        'Upload Files',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ),
-                );
-                return null;
-              }
-
-              final path = results.files.single.path!;
-              final fileName = results.files.single.name;
-
-              storage.uploadFile(path, fileName).then((value) => print("Done"));
-
-              print(path);
-              print(fileName);
-            },
-            child: Text("Upload Image")),
-      ]),
+                  SizedBox(height: 16),
+                  const Text(
+                    'Cloud Storage Homescreen',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: HomeScreen(),
+//     );
+//   }
+// }
+
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   _HomeScreenState createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   final FirebaseStorage storage = FirebaseStorage();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Birth Registration Image"),
+//       ),
+//       body: Column(children: [
+//         ElevatedButton(
+//             onPressed: () async {
+//               final results = await FilePicker.platform.pickFiles(
+//                 allowMultiple: false,
+//                 type: FileType.custom,
+//                 allowedExtensions: ['png', 'jpg', 'jpeg'],
+//               );
+
+//               if (results == null) {
+//                 ScaffoldMessenger.of(context).showSnackBar(
+//                   const SnackBar(
+//                     content: Text("No file selected"),
+//                   ),
+//                 );
+//                 return null;
+//               }
+
+//               final path = results.files.single.path!;
+//               final fileName = results.files.single.name;
+
+//               storage.uploadFile(path, fileName).then((value) => print("Done"));
+
+//               print(path);
+//               print(fileName);
+//             },
+//             child: Text("Upload Image")),
+//       ]),
+//     );
+//   }
+// }
